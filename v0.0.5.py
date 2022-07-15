32b958d7c204f50954ee29c513e9a23dada0e75f
PythonicHTML <98992286+PythonicHTML@users.noreply.github.com>
2022-07-14 21:22:22 -0400

    Update v0.0.5.py

--- v0.0.5.py
+++ v0.0.5.py
@@ -1,22 +1,23 @@
-''' Version 0.0.5 released April 26th, 2022
-No external modules currently required. Runs on Python versions 2.7 and 3.5 or above. '''
+''' Version 0.0.6 released April 26th, 2022
+No external modules currently required. Runs on Python versions 3.5 or above. '''
 
 
 import random as r
-head_list = ["Red Eye", "Blue Eye", "Yellow Eye", "Green Eye", "Red Shades", "Purple Shades", "Yellow Shades", "Shades", "Black Scarf", "Scarf and Shades", "Red Scarf"]
+head_list = ["White fire", "Red fire", "Green fire", "Blue fire", "Yellow fire", "Pink fire", "Orange fire", "Cowboy", "Brown cowboy hat", "Black cowboy hat", "Cowboy/black", "Roundshades - black", "Roundshades - green", "Roundshades - pink", "Roundshades - red", "Roundshades - yellow", "Roundshades - blue", "Shades - green", "Shades - blue", "Shades - orange", "Pirate bandana", "Combat helmet", "Combat helmet winter", "Combat helmet jungle", "Combat helmet desert", "Red Eye", "Blue Eye", "Yellow Eye", "Green Eye", "Red Shades", "Purple Shades", "Yellow Shades", "Shades", "Black Scarf", "Scarf and Shades", "Red Scarf"]
 hair_list = ["Long", "Goku", "Dreads", "Frizzy", "Conan", "Short", "Spikes", "Straight", "Wild", "Surfer", "Slick", "Messy", "Punk", "Santa"]
-torso = ["Ak-47 Ammo", "M16 Ammo", "Ak-47 Dual", "M16 Dual", "RPG Ammo", "Linkgun Ammo", "M79 Ammo", "Camo", "Camo 2", "Camo Ammo", "Camo 2 Ammo", "Tech 1", "Tech 2", "Tech 3", "Snow Camo", "Snow Camo RPG", "Shuriken", "Silver Chain", "Gold Chain", "White X", "Blue X", "Red X", "Green X", "Yellow X", "Purple Camo", "Fire Camo", "Aqua Camo", "Brown Camo"]
-right_shoulder = ["Camoflague", "Shoulderpad", "Arm Plate", "Snow Camo", "Camo Purple", "Camo Fire", "Camo Aqua", "Camo Brown"]
-left_shoulder = ["Camoflague", "Shoulderpad", "Arm Plate", "Snow Camo", "Camo Purple", "Camo Fire", "Camo Aqua", "Camo Brown"]
-right_hand = ["Wolverine", "Glove Grey", "Glove Brown", "Glove Metal", "Camo", "Wrist Blue", "Wrist Green", "Wrist Yellow", "Wrist Red", "Wrist Pink", "Wrist Navy", "Glove Red", "Glove Yellow", "Glove Marine", "Glove Pink", "Glove Green", "Plate", "Camo Snow", "Camo Purple", "Camo Fire", "Camo Aqua", "Camo Brown"]
-left_hand = ["Wolverine", "Glove Grey", "Glove Brown", "Glove Metal", "Camo", "Wrist Blue", "Wrist Green", "Wrist Yellow", "Wrist Red", "Wrist Pink", "Wrist Navy", "Glove Red", "Glove Yellow", "Glove Marine", "Glove Pink", "Glove Green", "Plate", "Camo Snow", "Camo Purple", "Camo Fire", "Camo Aqua", "Camo Brown"]
-belt = ["Belt Metal", "Belt Camo", "Belt Plate Full", "Belt Camo Full", "Belt Brown", "Camo Snow", "Belt Purple", "Belt Fire", "Belt Aqua", "Belt Brown Full"]
-right_leg = ["Plates", "Camo", "Snow Camo", "Purple Camo", "Fire", "Aqua", "Brown Camo"]
-left_leg = ["Plates", "Camo", "Snow Camo", "Purple Camo", "Fire", "Aqua", "Brown Camo"]
-shell = ["Storm", "Bubble", "Fractal"]
+torso = ["Desert camo", "Winter camo", "Jungle camo", "Vest 1", "Ak-47 Ammo", "M16 Ammo", "Ak-47 Dual", "M16 Dual", "RPG Ammo", "Linkgun Ammo", "M79 Ammo", "Camo", "Camo 2", "Camo Ammo", "Camo 2 Ammo", "Tech 1", "Tech 2", "Tech 3", "Snow Camo", "Snow Camo RPG", "Shuriken", "Silver Chain", "Gold Chain", "White X", "Blue X", "Red X", "Green X", "Yellow X", "Purple Camo", "Fire Camo", "Aqua Camo", "Brown Camo"]
+right_shoulder = ["Winter camo", "Desert camo", "Jungle camo", "Camoflague", "Shoulderpad", "Arm Plate", "Snow Camo", "Camo Purple", "Camo Fire", "Camo Aqua", "Camo Brown"]
+left_shoulder = ["Winter camo", "Desert camo", "Jungle camo", "Camoflague", "Shoulderpad", "Arm Plate", "Snow Camo", "Camo Purple", "Camo Fire", "Camo Aqua", "Camo Brown"]
+right_hand = ["Winter camo", "Jungle camo", "Desert camo", "Blade", "Twin blade", "Wrist - winter", "Wrist - jungle", "Wrist - desert", "Wolverine", "Glove Grey", "Glove Brown", "Glove Metal", "Camo", "Wrist Blue", "Wrist Green", "Wrist Yellow", "Wrist Red", "Wrist Pink", "Wrist Navy", "Glove Red", "Glove Yellow", "Glove Marine", "Glove Pink", "Glove Green", "Plate", "Camo Snow", "Camo Purple", "Camo Fire", "Camo Aqua", "Camo Brown"]
+left_hand = ["Winter camo", "Jungle camo", "Desert camo", "Blade", "Twin blade", "Wrist - winter", "Wrist - jungle", "Wrist - desert", "Wolverine", "Glove Grey", "Glove Brown", "Glove Metal", "Camo", "Wrist Blue", "Wrist Green", "Wrist Yellow", "Wrist Red", "Wrist Pink", "Wrist Navy", "Glove Red", "Glove Yellow", "Glove Marine", "Glove Pink", "Glove Green", "Plate", "Camo Snow", "Camo Purple", "Camo Fire", "Camo Aqua", "Camo Brown"]
+belt = ["Jungle camo", "Winter camo", "Desert camo", "Belt Metal", "Belt Camo", "Belt Plate Full", "Belt Camo Full", "Belt Brown", "Camo Snow", "Belt Purple", "Belt Fire", "Belt Aqua", "Belt Brown Full"]
+right_leg = ["Winter camo", "Jungle camo", "Desert camo", "Plates", "Camo", "Snow Camo", "Purple Camo", "Fire", "Aqua", "Brown Camo"]
+left_leg = ["Winter camo", "Jungle camo", "Desert camo", "Plates", "Camo", "Snow Camo", "Purple Camo", "Fire", "Aqua", "Brown Camo"]
+shell = ["Storm", "Bubble", "Fractal", "Dragon", "Jet feet", "Energy vacuum", "Jet feet 2"]
 shell_color = ["Blue", "Red", "Pink", "Magenta", "Turquoise", "Green", "Teal", "Cyan", "Orange", "Navy"]
 right_foot = ["Spikes", "Plates", "Boot", "Boot 2", "Rings", "Rings Gold", "Rings Pink", "Rings Green", "Rings Red", "Wrap", "Camo Wrap", "Wrap Red", "Wrap Blue", "Wrap Gold", "Wrap Green", "Boot Black"]
 left_foot = ["Spikes", "Plates", "Boot", "Boot 2", "Rings", "Rings Gold", "Rings Pink", "Rings Green", "Rings Red", "Wrap", "Camo Wrap", "Wrap Red", "Wrap Blue", "Wrap Gold", "Wrap Green", "Boot Black"]
+cape_list = ["Black", "Green", "Red", "Yellow", "Blue", "Pink", "Orange", "BW camo", "Jungle camo", "Winter camo", "Desert camo"]
 def main(shoulders=True, hands=True, feet=True):
     my_head = r.choice(head_list)
     my_hair = r.choice(hair_list)
@@ -44,6 +45,7 @@ def main(shoulders=True, hands=True, feet=True):
         my_other_foot = my_foot
     else:
         my_other_foot = r.choice(left_foot)
+    cape = r.choice(cape_list)
     print("Face: ", my_head)
     print("Hair Style: ", my_hair)
     print("Torso: ", my_torso)
@@ -58,4 +60,5 @@ def main(shoulders=True, hands=True, feet=True):
     print("Flight aura color: ", my_color)
     print("Right foot: ", my_foot)
     print("Left foot: ", my_other_foot)
+    print("Cape: ", cape)
 main()
